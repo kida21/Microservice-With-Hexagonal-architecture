38303e3059b954d7fedff0bf8388210cca00a04b
kida21 <kidusp22@gmail.com>
2025-05-09 09:43:34 +0300

    Service Registration

---
+++ common/registration.go
@@ -0,0 +1,47 @@
+package common
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/hashicorp/consul/api"
+)
+type Serviceregistrar struct{
+   Client *api.Client
+}
+
+func NewServiceRegistrar(addr string)(*Serviceregistrar){
+   config:= api.DefaultConfig()
+   config.Address=addr
+   client,err:=api.NewClient(config)
+   if err!=nil{
+	  log.Fatal(err)
+   }
+   return &Serviceregistrar{Client: client}
+}
+func generateServiceId(servicName string)(string){
+  id:=uuid.New().String()
+  return fmt.Sprintf("%s-%s",servicName,id)
+}
+
+func (s*Serviceregistrar) Register(serviceName,address string,port int)(string,error){
+   serviceId:=generateServiceId(serviceName)
+   registration:=&api.AgentServiceRegistration{
+	ID: serviceId,
+	Name: serviceName,
+	Port: port,
+	Address: address,
+    Check: &api.AgentServiceCheck{
+		GRPC: fmt.Sprintf("%s:%d",address,port),
+		Interval: "10s",
+		Timeout: "3s",
+		DeregisterCriticalServiceAfter: "2m",
+	},
+   } 
+   err:=s.Client.Agent().ServiceRegister(registration)
+   if err!=nil{
+	log.Fatalf("failed to register a service:%v",err)
+   }
+   return serviceId,nil
+}
